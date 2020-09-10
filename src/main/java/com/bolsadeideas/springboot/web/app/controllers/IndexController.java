d2d260aee17c786d67d762b71175578d362d11b9
JuanEs <12g4ru12@gmail.com>
2020-09-10 01:06:23 -0500

    Creando el package models y class Usuario

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
+    /**
+     * metodo de prueba
+     * @param model se utiliza para pasar parametros a la vista con clave y valor.
+     * @return
+     */
     @GetMapping({"/index", "/", "/home", ""})
     public String index(Model model){
         model.addAttribute("titulo", "hola Spring Framework con Model!");
