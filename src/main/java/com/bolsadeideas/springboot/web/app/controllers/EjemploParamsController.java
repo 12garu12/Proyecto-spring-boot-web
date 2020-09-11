99c03f84ca36acea672fb9a6c315cadaf21b125e
JuanEs <12g4ru12@gmail.com>
2020-09-10 19:14:59 -0500

    Obtener varios parametros con @RequestParam CursoV20 y HttpServletRequest y envio thymeleaf

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
 
+    /**
+     * Metodo para retornar una vista html.
+     * @return una vista llamada index.html que se encuentra en el folder params.
+     */
     @GetMapping("/")
     public String index(){
         return "/params/index";
@@ -29,6 +35,42 @@ public class EjemploParamsController {
         model.addAttribute("resultado", "El texto enviado es: " + texto);
         return "params/ver";
     }
+
+    /**
+     * Metodo para recibir varios parametros por medio de la URL.
+     * @param saludo parametro que llega por medio de la URL.
+     * @param numero parametro que llega por medio de la URL.
+     * @param model utiliza los metodos de la interfaz Model para pasar datos clave valor.
+     * @return un archivo html llamado ver.html.
+     */
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model){
+        model.addAttribute("resultado", "El saludo enviado es: '" +
+                saludo + "' y el numero es: '" + numero + "'");
+        return "params/ver";
+
+    }
+
+    /**
+     * Metodo para recibir varios parametros por medio de la URL.
+     * @param request trae los parametros al metodo.
+     * @param model utiliza los metodos de la interfaz Model para pasar datos clave valor.
+     * @return un archivo html llamado ver.html.
+     */
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){ // la interface HttpServletRequest trae los parametros en un array y el metodo getParameter() lo obtinen.
+        String saludo = request.getParameter("saludo");
+        Integer numero = null;
+        try {
+            numero = Integer.parseInt(request.getParameter("numero")) ;
+        } catch (NumberFormatException e){
+            numero = 0;
+        }
+        model.addAttribute("resultado", "El saludo enviado es: '" +
+                saludo + "' y el numero es: '" + numero + "'");
+        return "params/ver";
+    }
+
 
 
 }
